c511894b981ed0eefb9fcd2cf2036e1dfcd91766
Даниил Чернышов <Dan24012004@yandex.ru>
2026-02-18 00:16:25 +0300

    исправил курсовой проект 4, жду оценки(18/02)

---
+++ mailings/management/__init__.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from . import views
+
+app_name = 'mailings'
+
+urlpatterns = [
+
+    path('', views.index, name='home'),
+
+    # Клиенты
+    path('clients/', views.ClientListView.as_view(), name='client_list'),
+    path('clients/create/', views.ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+
+    # Сообщения
+    path('messages/', views.MessageListView.as_view(), name='message_list'),
+    path('messages/create/', views.MessageCreateView.as_view(), name='message_create'),
+    path('messages/<int:pk>/update/', views.MessageUpdateView.as_view(), name='message_update'),
+    path('messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message_delete'),
+
+    # Рассылки
+    path('mailings/', views.MailingListView.as_view(), name='mailing_list'),
+    path('mailings/create/', views.MailingCreateView.as_view(), name='mailing_create'),
+    path('mailings/<int:pk>/', views.MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailings/<int:pk>/update/', views.MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/<int:pk>/delete/', views.MailingDeleteView.as_view(), name='mailing_delete'),
+    path('mailings/<int:pk>/send/', views.send_mailing_view, name='send_mailing'),
+
+    # Попытки рассылок
+    path('attempts/', views.MailingAttemptListView.as_view(), name='attempt_list'),
+
+    # Отчёты
+    path('reports/', views.reports, name='reports'),
+]
